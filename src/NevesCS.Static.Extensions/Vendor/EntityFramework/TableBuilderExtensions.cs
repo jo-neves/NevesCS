f22539bf6b852a46226b72edb9352f5917f3b105
João Neves <joao95neves@gmail.com>
2024-07-19 10:29:32 +0100

    fix(SqlUtils): multiple fixes

--- src/NevesCS.Static.Extensions/Vendor/EntityFramework/TableBuilderExtensions.cs
+++ src/NevesCS.Static.Extensions/Vendor/EntityFramework/TableBuilderExtensions.cs
@@ -6,32 +6,14 @@ namespace NevesCS.Static.Extensions.Vendor.EntityFramework
 {
     public static class TableBuilderExtensions
     {
-        /// <summary>
-        /// R≥0={x∈R∣x≥0}
-        ///
-        /// </summary>
-        public static TableBuilder<TEntity> ConfigurePropertyAsNonNegativeRealNumber<TEntity>(
+        public static TableBuilder<TEntity> ConfigurePropertyAsPositive<TEntity>(
             this TableBuilder<TEntity> builder,
             string tableName,
             string propertyName)
 
             where TEntity : class
         {
-            return TableBuilderUtils.ConfigurePropertyAsNonNegativeRealNumber(builder, tableName, propertyName);
-        }
-
-        /// <summary>
-        /// R>0={x∈R∣x>0}
-        ///
-        /// </summary>
-        public static TableBuilder<TEntity> ConfigurePropertyAsPositiveRealNumber<TEntity>(
-            this TableBuilder<TEntity> builder,
-            string tableName,
-            string propertyName)
-
-            where TEntity : class
-        {
-            return TableBuilderUtils.ConfigurePropertyAsPositiveRealNumber(builder, tableName, propertyName);
+            return TableBuilderUtils.ConfigurePropertyAsPositive(builder, tableName, propertyName);
         }
     }
 }
--- src/NevesCS.Static/Utils/Vendor/EntityFramework/TableBuilderUtils.cs
+++ src/NevesCS.Static/Utils/Vendor/EntityFramework/TableBuilderUtils.cs
@@ -4,34 +4,14 @@ namespace NevesCS.Static.Utils.Vendor.EntityFramework
 {
     public static class TableBuilderUtils
     {
-        /// <summary>
-        /// R≥0={x∈R∣x≥0}
-        ///
-        /// </summary>
-        public static TableBuilder<TEntity> ConfigurePropertyAsNonNegativeRealNumber<TEntity>(
+        public static TableBuilder<TEntity> ConfigurePropertyAsPositive<TEntity>(
             TableBuilder<TEntity> builder,
             string tableName,
             string propertyName)
 
             where TEntity : class
         {
-            builder.HasCheckConstraint($"CK_{tableName}_{propertyName}_NonNegativeRealNumber", $"{propertyName} >= 0");
-
-            return builder;
-        }
-
-        /// <summary>
-        /// R>0={x∈R∣x>0}
-        ///
-        /// </summary>
-        public static TableBuilder<TEntity> ConfigurePropertyAsPositiveRealNumber<TEntity>(
-            TableBuilder<TEntity> builder,
-            string tableName,
-            string propertyName)
-
-            where TEntity : class
-        {
-            builder.HasCheckConstraint($"CK_{tableName}_{propertyName}_PositiveRealNumber", $"{propertyName} > 0");
+            builder.HasCheckConstraint($"CK_{tableName}_{propertyName}_Positive", $"{propertyName} >= 0");
 
             return builder;
         }
