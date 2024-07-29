ff871ff9290afa799750c254a9cfda959bf69303
João Neves <joao95neves@gmail.com>
2024-07-29 14:51:47 +0100

    feat(Web3): created a wrapper for Solana HTTP client

--- src/NevesCS.Abstractions/Clients/Web3/Solana/ISolanaClient.cs
+++ src/NevesCS.Abstractions/Clients/Web3/Solana/ISolanaClient.cs
@@ -2,6 +2,6 @@ namespace NevesCS.Abstractions.Clients.Web3.Solana
 {
     public interface ISolanaClient
     {
-        public Task<bool> CheckAndWaitForTransactionConfirmedAsync(string transactionSignature);
+        public Task<bool> CheckForTransactionConfirmedAsync(string transactionSignature);
     }
 }
