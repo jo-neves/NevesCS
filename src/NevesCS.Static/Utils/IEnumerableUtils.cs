cf9da0ff58c8733dd922ce1576e48754692e9855
João Neves <joao95neves@gmail.com>
2024-07-29 16:11:16 +0100

    feat(IEnumerableUtils): added `.OrEmpty()`

--- src/NevesCS.Static/Utils/IEnumerableUtils.cs
+++ src/NevesCS.Static/Utils/IEnumerableUtils.cs
@@ -1,52 +1,48 @@
-namespace NevesCS.Static.Utils;
-
-public static class IEnumerableUtils
+namespace NevesCS.Static.Utils
 {
-    /// <summary>
-    /// It materializes/consumes the enumerable and performs a foreach in it.
-    ///
-    /// </summary>
-    public static void ForEach<T>(IEnumerable<T> enumeration, Action<T> action)
+    public static class IEnumerableUtils
     {
-        Array.ForEach([.. enumeration], action);
-    }
-
-    public static bool None<T>(IEnumerable<T> enumeration)
-    {
-        return !enumeration.Any();
-    }
-
-    public static bool None<T>(IEnumerable<T> enumeration, Func<T, bool> predicate)
-    {
-        return !enumeration.Any(predicate);
-    }
-
-    public static T? TryGetElementAtOr<T>(IEnumerable<T> enumeration, Index index, T? defaultValue = default)
-    {
-        try
+        /// <summary>
+        /// It materializes/consumes the enumerable and performs a foreach in it.
+        ///
+        /// </summary>
+        public static void ForEach<T>(IEnumerable<T> enumeration, Action<T> action)
         {
-            return enumeration.ElementAt(index);
+            Array.ForEach(enumeration.ToArray(), action);
         }
-        catch (IndexOutOfRangeException)
+
+        public static bool None<T>(IEnumerable<T> enumeration)
         {
-            return defaultValue;
+            return !enumeration.Any();
         }
-    }
 
-    public static bool AreAnyNull<T>(IEnumerable<T> values)
-    {
-        return values.Any(value => value is null);
-    }
+        public static bool None<T>(IEnumerable<T> enumeration, Func<T, bool> predicate)
+        {
+            return !enumeration.Any(predicate);
+        }
 
-    public static IEnumerable<TValue> OrEmpty<TValue>(IEnumerable<TValue>? enumeration)
-    {
-        return ObjectUtils.IsNull(enumeration)
-            ? []
-            : enumeration!;
-    }
+        public static T? TryGetElementAtOr<T>(IEnumerable<T> enumeration, Index index, T? defaultValue = default)
+        {
+            try
+            {
+                return enumeration.ElementAt(index);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return defaultValue;
+            }
+        }
 
-    public static bool ContainsObjectValue<TObject, TValue>(IEnumerable<TValue> enumeration, TObject testObject, Func<TObject, TValue> selector)
-    {
-        return enumeration.Contains(selector(testObject));
+        public static IEnumerable<TValue> OrEmpty<TValue>(IEnumerable<TValue>? enumeration)
+        {
+            return ObjectUtils.IsNull(enumeration)
+                ? []
+                : enumeration!;
+        }
+
+        public static bool ContainsObjectValue<TObject, TValue>(IEnumerable<TValue> enumeration, TObject testObject, Func<TObject, TValue> selector)
+        {
+            return enumeration.Contains(selector(testObject));
+        }
     }
 }
