8b2ec4e35e7c42ea519d7abe93aeeedd61f1a0cd
João Neves <joao95neves@gmail.com>
2024-07-29 15:19:18 +0100

    !refactor(ReflectionUtils): extracted methods from `ObjectUtils`

--- src/NevesCS.Static/Utils/ReflectionUtils.cs
+++ src/NevesCS.Static/Utils/ReflectionUtils.cs
@@ -40,73 +40,9 @@ namespace NevesCS.Static.Utils
                                                          ? (MemberExpression)unaryLambdaExpression.Operand
                                                          : (MemberExpression)lambdaExpression.Body)
                                                      .Member,
+
                 _ => null,
             };
-        }
-
-        /// <summary>
-        /// Performs a search through all types in an external assembly, filtering by a predicate.
-        ///
-        /// </summary>
-        /// <param name="assemblyPath">E.g.: "path/to/your/project.dll"</param>
-        /// <param name="wherePredicate"></param>
-        /// <returns></returns>
-        public static IEnumerable<Type> GetAllTypesFromExternalAssembly(string assemblyPath, Func<Type, bool> wherePredicate)
-        {
-            var assembly = Assembly.LoadFrom(assemblyPath);
-
-            return assembly
-                .GetTypes()
-                .Where(wherePredicate);
-        }
-
-        /// <summary>
-        /// Performs a search through all types in the assembly that contains <typeparamref name="KnownType"/>, filtering by a predicate.
-        ///
-        /// </summary>
-        /// <typeparam name="KnownType">A known type from the assembly to perform the search in.</typeparam>
-        /// <param name="wherePredicate"></param>
-        /// <returns></returns>
-        public static IEnumerable<Type> GetAllTypesFrom<KnownType>(Func<Type, bool> wherePredicate)
-        {
-            return typeof(KnownType)
-                .Assembly
-                .GetTypes()
-                .Where(wherePredicate);
-        }
-
-        public static Type? GetTypeByNameFromAssembly(Assembly assembly, string typeName)
-        {
-            return assembly
-                .GetTypes()
-                .FirstOrDefault(type => type.Name == typeName);
-        }
-
-        /// <summary>
-        /// Gets a specific Type by name, supporting external assemblies.
-        ///
-        /// </summary>
-        /// <typeparam name="KnownType">A known type embedded in the Assembly where the <paramref name="typeName"/> belongs to.</typeparam>
-        public static Type GetTypeByNameFrom<KnownType>(string typeName)
-        {
-            return typeof(KnownType)
-                .Assembly
-                .DefinedTypes
-                .First(type => type.Name == typeName)
-                .AsType();
-        }
-
-        /// <summary>
-        /// Gets a specific Type by name from an external assembly.
-        ///
-        /// </summary>
-        public static Type GetTypeByNameFromExternalAssembly(string assemblyPath, string typeName)
-        {
-            var assembly = Assembly.LoadFrom(assemblyPath);
-
-            return assembly
-                .GetTypes()
-                .First(type => type.Name == typeName);
         }
 
         /// <summary>
