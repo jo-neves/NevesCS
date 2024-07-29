11e5f76eccdd8f72feec043179875ece175478ef
João Neves <joao95neves@gmail.com>
2024-07-29 16:12:13 +0100

    feat(Traits): added new traits

--- src/NevesCS.Abstractions/Traits/IAuditableEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace NevesCS.Abstractions.Traits
-{
-    public interface IAuditableEntity
-    {
-        public DateTimeOffset CreationDate { get; }
-
-        public DateTimeOffset LastUpdateDate { get; set; }
-    }
-}
