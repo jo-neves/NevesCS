7d57fb1f47ca56cecc856089809b29b8ee9274b6
Jo Neves <joao95neves@gmail.com>
2025-12-05 16:57:23 +0000

    feat(Static): adds `.DecimalToCents()` and `.CentsToDecimal()` to `CurrencyConversionUtilsTests`

--- src/NevesCS.Static/Utils/CurrencyConversionUtils.cs
+++ src/NevesCS.Static/Utils/CurrencyConversionUtils.cs
@@ -1,17 +1,14 @@
-using NevesCS.Static.Constants.Values;
-
 namespace NevesCS.Static.Utils;
 
 public static class CurrencyConversionUtils
 {
     public static long DecimalToCents(this decimal value)
     {
-        return decimal.ToInt64(
-            decimal.Round(value * Decimals.OneHundred, MidpointRounding.AwayFromZero));
+        return decimal.ToInt64(decimal.Round(value * 100m, MidpointRounding.AwayFromZero));
     }
 
     public static decimal CentsToDecimal(this long value)
     {
-        return value / Decimals.OneHundred;
+        return value / 100m;
     }
 }
