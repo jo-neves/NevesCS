691045ea37bee732fff8d60fc5588c103b45095d
Jo Neves <joao95neves@gmail.com>
2026-05-15 13:48:39 +0100

    feat(Static): adds `PollyRetryPolicyUtils`

---
+++ src/NevesCS.Static/Utils/Vendor/PollyRetryPolicyUtils.cs
@@ -0,0 +1,28 @@
+using Polly;
+using Polly.Retry;
+
+namespace NevesCS.Static.Utils.Vendor
+{
+    public static class PollyRetryPolicyUtils
+    {
+        public static AsyncRetryPolicy GetHttpRetryPolicy(int retryCount, int startMillisecondsDuration)
+        {
+            return Policy
+                .Handle<HttpRequestException>()
+                .WaitAndRetryAsync(GetSleepDurationPolicy(retryCount, startMillisecondsDuration));
+        }
+
+        public static IEnumerable<TimeSpan> GetSleepDurationPolicy(int numberOfRetries, int startMillisecondsDuration)
+        {
+            for (var i = 1; i <= numberOfRetries; ++i)
+            {
+                yield return GetSleepDuration(startMillisecondsDuration, i);
+            }
+        }
+
+        public static TimeSpan GetSleepDuration(int startMillisecondsDuration, int retryAttempt)
+        {
+            return TimeSpan.FromSeconds(startMillisecondsDuration * retryAttempt);
+        }
+    }
+}
