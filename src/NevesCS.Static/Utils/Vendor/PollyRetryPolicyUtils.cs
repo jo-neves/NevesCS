00c43f7e9892213dab2ee9fc6f1303270a77d53d
Jo Neves <joao95neves@gmail.com>
2026-06-03 17:23:29 +0100

    !style(PollyRetryPolicyUtils): renames ".GetHttpRetryPolicy()" into ".GetHttpAsyncRetryPolicy()"

--- src/NevesCS.Static/Utils/Vendor/PollyRetryPolicyUtils.cs
+++ src/NevesCS.Static/Utils/Vendor/PollyRetryPolicyUtils.cs
@@ -1,28 +1,27 @@
 using Polly;
 using Polly.Retry;
 
-namespace NevesCS.Static.Utils.Vendor
+namespace NevesCS.Static.Utils.Vendor;
+
+public static class PollyRetryPolicyUtils
 {
-    public static class PollyRetryPolicyUtils
+    public static AsyncRetryPolicy GetHttpAsyncRetryPolicy(int retryCount, int startMillisecondsDuration)
     {
-        public static AsyncRetryPolicy GetHttpRetryPolicy(int retryCount, int startMillisecondsDuration)
-        {
-            return Policy
-                .Handle<HttpRequestException>()
-                .WaitAndRetryAsync(GetSleepDurationPolicy(retryCount, startMillisecondsDuration));
-        }
+        return Policy
+            .Handle<HttpRequestException>()
+            .WaitAndRetryAsync(GetSleepDurationPolicy(retryCount, startMillisecondsDuration));
+    }
 
-        public static IEnumerable<TimeSpan> GetSleepDurationPolicy(int numberOfRetries, int startMillisecondsDuration)
+    public static IEnumerable<TimeSpan> GetSleepDurationPolicy(int numberOfRetries, int startMillisecondsDuration)
+    {
+        for (var i = 1; i <= numberOfRetries; ++i)
         {
-            for (var i = 1; i <= numberOfRetries; ++i)
-            {
-                yield return GetSleepDuration(startMillisecondsDuration, i);
-            }
+            yield return GetSleepDuration(startMillisecondsDuration, i);
         }
+    }
 
-        public static TimeSpan GetSleepDuration(int startMillisecondsDuration, int retryAttempt)
-        {
-            return TimeSpan.FromSeconds(startMillisecondsDuration * retryAttempt);
-        }
+    public static TimeSpan GetSleepDuration(int startMillisecondsDuration, int retryAttempt)
+    {
+        return TimeSpan.FromSeconds(startMillisecondsDuration * retryAttempt);
     }
 }
