b71ac61736157e3d32d0c39ca80aedf570f46f22
Jo Neves <joao95neves@gmail.com>
2026-06-12 17:32:04 +0100

    feat(DictionaryUtils): adds `.ToConcurrentDictionary()`

--- src/NevesCS.Static.Extensions/DictionaryExtensions.cs
+++ src/NevesCS.Static.Extensions/DictionaryExtensions.cs
@@ -1,75 +1,84 @@
 using NevesCS.Static.Utils;
 
-namespace NevesCS.Static.Extensions
+using System.Collections.Concurrent;
+
+namespace NevesCS.Static.Extensions;
+
+public static class DictionaryExtensions
 {
-    public static class DictionaryExtensions
+    public static IDictionary<TKey, TValue> CloneIntoNew<TKey, TValue>(this IDictionary<TKey, TValue> target)
+        where TKey : notnull
     {
-        public static IDictionary<TKey, TValue> CloneIntoNew<TKey, TValue>(this IDictionary<TKey, TValue> target)
-            where TKey : notnull
-        {
-            return DictionaryUtils.CloneIntoNew(target);
-        }
+        return DictionaryUtils.CloneIntoNew(target);
+    }
 
-        public static TValue? GetValueOr<TKey, TValue>(
-            this IDictionary<TKey, TValue> target,
-            TKey key,
-            TValue? defaultValue = default)
-        {
-            return DictionaryUtils.GetValueOr(target, key, defaultValue);
-        }
+    public static TValue? GetValueOr<TKey, TValue>(
+        this IDictionary<TKey, TValue> target,
+        TKey key,
+        TValue? defaultValue = default)
+    {
+        return DictionaryUtils.GetValueOr(target, key, defaultValue);
+    }
 
-        /// <summary>
-        /// Gets an item by key, or adds a new <typeparamref name="TValue"/> instance.
-        ///
-        /// </summary>
-        public static TValue GetOrCreate<TKey, TValue>(
-            this IDictionary<TKey, TValue> target,
-            TKey key)
+    /// <summary>
+    /// Gets an item by key, or adds a new <typeparamref name="TValue"/> instance.
+    ///
+    /// </summary>
+    public static TValue GetOrCreate<TKey, TValue>(
+        this IDictionary<TKey, TValue> target,
+        TKey key)
 
-            where TValue : new()
-        {
-            return DictionaryUtils.GetOrCreate(target, key);
-        }
+        where TValue : new()
+    {
+        return DictionaryUtils.GetOrCreate(target, key);
+    }
 
-        /// <summary>
-        /// Gets an item by key, or adds a new value from the <paramref name="valueFactory"/>.
-        ///
-        /// </summary>
-        public static TValue GetOrCreate<TKey, TValue>(
-            this IDictionary<TKey, TValue> target,
-            TKey key,
-            Func<TValue> valueFactory)
-        {
-            return DictionaryUtils.GetOrCreate(target, key, valueFactory);
-        }
+    /// <summary>
+    /// Gets an item by key, or adds a new value from the <paramref name="valueFactory"/>.
+    ///
+    /// </summary>
+    public static TValue GetOrCreate<TKey, TValue>(
+        this IDictionary<TKey, TValue> target,
+        TKey key,
+        Func<TValue> valueFactory)
+    {
+        return DictionaryUtils.GetOrCreate(target, key, valueFactory);
+    }
 
-        /// <summary>
-        /// Get an item by key, or adds a new return value from the <paramref name="asyncValueFactory"/>.
-        ///
-        /// </summary>
-        public static async Task<TValue> GetOrCreateAsync<TKey, TValue>(
-            this IDictionary<TKey, TValue> target,
-            TKey key,
-            Func<Task<TValue>> asyncValueFactory)
-        {
-            return await DictionaryUtils.GetOrCreateAsync(target, key, asyncValueFactory);
-        }
+    /// <summary>
+    /// Get an item by key, or adds a new return value from the <paramref name="asyncValueFactory"/>.
+    ///
+    /// </summary>
+    public static async Task<TValue> GetOrCreateAsync<TKey, TValue>(
+        this IDictionary<TKey, TValue> target,
+        TKey key,
+        Func<Task<TValue>> asyncValueFactory)
+    {
+        return await DictionaryUtils.GetOrCreateAsync(target, key, asyncValueFactory);
+    }
 
-        public static IDictionary<TKey, TValue> Upsert<TKey, TValue>(
-            this IDictionary<TKey, TValue> target,
-            TKey key,
-            TValue value)
+    public static IDictionary<TKey, TValue> Upsert<TKey, TValue>(
+        this IDictionary<TKey, TValue> target,
+        TKey key,
+        TValue value)
 
-            where TValue : new()
-        {
-            return DictionaryUtils.Upsert(target, key, value);
-        }
+        where TValue : new()
+    {
+        return DictionaryUtils.Upsert(target, key, value);
+    }
 
-        public static void UpdateOrCreate<TKey, TValue>(
-            this IDictionary<TKey, TValue> target,
-            IDictionary<TKey, TValue> source)
-        {
-            DictionaryUtils.UpdateOrCreate(target, source);
-        }
+    public static void UpdateOrCreate<TKey, TValue>(
+        this IDictionary<TKey, TValue> target,
+        IDictionary<TKey, TValue> source)
+    {
+        DictionaryUtils.UpdateOrCreate(target, source);
+    }
+
+    public static ConcurrentDictionary<TKey, TValue> ToConcurrentDictionary<TKey, TValue>(
+        this IDictionary<TKey, TValue> source)
+
+        where TKey : notnull
+    {
+        return DictionaryUtils.ToConcurrentDictionary(source);
     }
 }
--- src/NevesCS.Static/Utils/DictionaryUtils.cs
+++ src/NevesCS.Static/Utils/DictionaryUtils.cs
@@ -1,96 +1,105 @@
-namespace NevesCS.Static.Utils
+using System.Collections.Concurrent;
+
+namespace NevesCS.Static.Utils;
+
+public static class DictionaryUtils
 {
-    public static class DictionaryUtils
+    public static IDictionary<TKey, TValue> CloneIntoNew<TKey, TValue>(IDictionary<TKey, TValue> target)
+        where TKey : notnull
     {
-        public static IDictionary<TKey, TValue> CloneIntoNew<TKey, TValue>(IDictionary<TKey, TValue> target)
-            where TKey : notnull
+        return new Dictionary<TKey, TValue>(target);
+    }
+
+    public static IDictionary<TKey, TValue> Upsert<TKey, TValue>(IDictionary<TKey, TValue> target, TKey key, TValue value)
+        where TValue : new()
+    {
+        target[key] = value;
+
+        return target;
+    }
+
+    public static TValue? GetValueOr<TKey, TValue>(
+        IDictionary<TKey, TValue> target,
+        TKey key,
+        TValue? defaultValue = default)
+    {
+        if (!target.TryGetValue(key, out TValue? existingValue))
         {
-            return new Dictionary<TKey, TValue>(target);
+            return defaultValue;
         }
 
-        public static IDictionary<TKey, TValue> Upsert<TKey, TValue>(IDictionary<TKey, TValue> target, TKey key, TValue value)
-            where TValue : new()
-        {
-            target[key] = value;
+        return existingValue;
+    }
 
-            return target;
+    /// <summary>
+    /// Gets an item by key, or adds a new <typeparamref name="TValue"/> instance.
+    ///
+    /// </summary>
+    public static TValue GetOrCreate<TKey, TValue>(
+        IDictionary<TKey, TValue> target,
+        TKey key)
+        where TValue : new()
+    {
+        if (!target.TryGetValue(key, out TValue? value))
+        {
+            value = new TValue();
+            target.Add(key, value);
         }
 
-        public static TValue? GetValueOr<TKey, TValue>(
-            IDictionary<TKey, TValue> target,
-            TKey key,
-            TValue? defaultValue = default)
-        {
-            if (!target.TryGetValue(key, out TValue? existingValue))
-            {
-                return defaultValue;
-            }
+        return value;
+    }
 
-            return existingValue;
+    /// <summary>
+    /// Gets an item by key, or adds a new value from the <paramref name="valueFactory"/>.
+    ///
+    /// </summary>
+    public static TValue GetOrCreate<TKey, TValue>(
+        IDictionary<TKey, TValue> target,
+        TKey key,
+        Func<TValue> valueFactory)
+    {
+        if (!target.TryGetValue(key, out TValue? value))
+        {
+            value = valueFactory();
+            target.Add(key, value);
         }
 
-        /// <summary>
-        /// Gets an item by key, or adds a new <typeparamref name="TValue"/> instance.
-        ///
-        /// </summary>
-        public static TValue GetOrCreate<TKey, TValue>(
-            IDictionary<TKey, TValue> target,
-            TKey key)
-            where TValue : new()
-        {
-            if (!target.TryGetValue(key, out TValue? value))
-            {
-                value = new TValue();
-                target.Add(key, value);
-            }
+        return value;
+    }
 
-            return value;
+    /// <summary>
+    /// Get an item by key, or adds a new return value from the <paramref name="asyncValueFactory"/>.
+    ///
+    /// </summary>
+    public static async Task<TValue> GetOrCreateAsync<TKey, TValue>(
+        IDictionary<TKey, TValue> target,
+        TKey key,
+        Func<Task<TValue>> asyncValueFactory)
+    {
+        if (!target.TryGetValue(key, out TValue? existingValue))
+        {
+            existingValue = await asyncValueFactory();
+            target.Add(key, existingValue);
         }
 
-        /// <summary>
-        /// Gets an item by key, or adds a new value from the <paramref name="valueFactory"/>.
-        ///
-        /// </summary>
-        public static TValue GetOrCreate<TKey, TValue>(
-            IDictionary<TKey, TValue> target,
-            TKey key,
-            Func<TValue> valueFactory)
+        return existingValue;
+    }
+
+    public static void UpdateOrCreate<TKey, TValue>(
+        IDictionary<TKey, TValue> target,
+        IDictionary<TKey, TValue> source)
+    {
+        foreach (var kvp in source)
         {
-            if (!target.TryGetValue(key, out TValue? value))
-            {
-                value = valueFactory();
-                target.Add(key, value);
-            }
-
-            return value;
+            target[kvp.Key] = kvp.Value;
         }
+    }
 
-        /// <summary>
-        /// Get an item by key, or adds a new return value from the <paramref name="asyncValueFactory"/>.
-        ///
-        /// </summary>
-        public static async Task<TValue> GetOrCreateAsync<TKey, TValue>(
-            IDictionary<TKey, TValue> target,
-            TKey key,
-            Func<Task<TValue>> asyncValueFactory)
-        {
-            if (!target.TryGetValue(key, out TValue? existingValue))
-            {
-                existingValue = await asyncValueFactory();
-                target.Add(key, existingValue);
-            }
+    public static ConcurrentDictionary<TKey, TValue> ToConcurrentDictionary<TKey, TValue>(
+        IDictionary<TKey, TValue> source)
 
-            return existingValue;
-        }
-
-        public static void UpdateOrCreate<TKey, TValue>(
-            IDictionary<TKey, TValue> target,
-            IDictionary<TKey, TValue> source)
-        {
-            foreach (var kvp in source)
-            {
-                target[kvp.Key] = kvp.Value;
-            }
-        }
+        where TKey : notnull
+    {
+        return new ConcurrentDictionary<TKey, TValue>(source);
     }
 }
